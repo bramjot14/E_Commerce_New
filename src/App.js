57e38a257f0dc4412b627c6d9c5b7b65a41971df
bramjot14 <bramjot9874@gmail.com>
2025-08-04 19:54:20 -0400

    Final HTTPS pusasasasasasah

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </div>
+  );
+}
+
+export default App;
