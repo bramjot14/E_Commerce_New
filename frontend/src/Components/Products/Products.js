ef8705ca61c62c0b5b28faca81e6bf91b67dc89a
bramjot14 <bramjot9874@gmail.com>
2025-08-20 04:15:37 -0400

    Added all popular products

--- frontend/src/Components/Products/Products.js
+++ frontend/src/Components/Products/Products.js
@@ -1,24 +1,73 @@
 export const products = [
     {
       id: 1,
-      name: "Macbook",
-      price: 99.99,
-      image: "../Assets/macbook.png",
-      description: "jasd dajbasjd asd asjddsa ashj sa as"
+      name: "Airpods",
+      price: 399.99,
+      image: "../Assets/airpods.png",
+      description: "Apple AirPods pro (2nd gen) c.."
     },
     {
       id: 2,
-      name: "Play Station 5",
-      price: 149.99,
-      image: "../Assets/ps5.png",
-      description: "jasd dajbasjd asd asjddsa ashj sa as"
+      name: "Canon EOS R5",
+      price: 3899.99,
+      image: "../Assets/cannon_camera.png",
+      description: "Canon EOS R5 – A professional he.."
     },
     {
       id: 3,
-      name: "Headphones",
-      price: 59.99,
-      image: "../Assets/headphones.png",
-      description: "jasd dajbasjd asd asjddsa ashj sa as"
-    }
+      name: "PlayStation 5",
+      price: 799.99,
+      image: "../Assets/playstation.png",
+      description: "Sony PlayStation 5 (PS5) – Next-gen.."
+    },
+    {
+      id: 4,
+      name: "Samsung Galaxy S23",
+      price: 799.99,
+      image: "../Assets/samsung_s23phone.png",
+      description: "A sleek flagship smartphone wi.."
+    },
+    {
+      id: 5,
+      name: "ASUS Rog Zephyrus G16",
+      price: 1999.99,
+      image: "../Assets/asus_laptop.png",
+      description: "ASUS ROG G16 – A powerful g.."
+    },
+    {
+      id: 6,
+      name: "Garmin Venu 2",
+      price: 349.99,
+      image: "../Assets/venu_watch.png",
+      description: "Garmin Venu 2 – A premium GP.."
+    },
+    {
+      id: 7,
+      name: "Sony WF-1000XM5",
+      price: 299.99,
+      image: "../Assets/sony_airbuds.png",
+      description: "Sony WF-1000XM5 – Premium wirele.."
+    },
+    {
+      id: 8,
+      name: "Samsung Projector 4K",
+      price: 1499.99,
+      image: "../Assets/projector.png",
+      description: "Samsung 4K Projector – A sleek a.."
+    },
+    {
+      id: 9,
+      name: "Bose QuietComfort 45",
+      price: 329.99,
+      image: "../Assets/bose_headphone.png",
+      description: "Bose QuietComfort 45 – wirele.."
+    },
+    {
+      id: 20,
+      name: "Macbook Pro 16",
+      price: 2499.99,
+      image: "../Assets/macbook16.png",
+      description: "Apple MacBook Pro 16-inch – A powerfu.."
+    },
   ];
   
