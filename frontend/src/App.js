fef715bd940ff7776a3d8838a7471d8fe8f71b20
bramjot14 <bramjot9874@gmail.com>
2025-08-21 07:52:37 -0400

    Created featured products section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import ProductList from './Components/Products/ProductList';
+import FeaturedProducts from './Components/FeaturedProducts/FeaturedProducts';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Hero></Hero>
-      <h3>Popular Products</h3>
+      <h3 className='popular'>Popular Products</h3>
       <ProductList></ProductList>
-
+      <h3 className='featured'>Featured Products</h3>
+      <FeaturedProducts></FeaturedProducts>
     </div>
   );
 }
---
+++ frontend/src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./FeaturedProducts.css"
+
+const FeaturedProducts = () => {
+  return (
+    <div className='container'>
+          <img className='a' src="../Assets/black.jpeg" alt=""/>
+  <div class="bottom-left">Bottom Left</div>
+  <div class="top-left">Top Left</div>
+  <div class="top-right">Top Right</div>
+  <div class="bottom-right">Bottom Right</div>
+  <div class="centered">Centered</div>
+    </div>
+  )
+}
+
+export default FeaturedProducts
