822f2565f80eb71f0ec8bb8d44269f56fcfa64c1
bramjot14 <bramjot9874@gmail.com>
2025-08-27 18:24:27 -0400

    Modified Featured Products section

--- frontend/src/Components/FeaturedProducts/FeaturedProducts.jsx
+++ frontend/src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -4,14 +4,34 @@ import "./FeaturedProducts.css"
 const FeaturedProducts = () => {
   return (
     <div className='container'>
-          <img className='a' src="../Assets/black.jpeg" alt=""/>
-  <div class="bottom-left">Bottom Left</div>
-  <div class="top-left">Top Left</div>
-  <div class="top-right">Top Right</div>
-  <div class="bottom-right">Bottom Right</div>
-  <div class="centered">Centered</div>
-    </div>
+      <div className="container2">
+          <img className='a' src="../Assets/girl_with_headphone_image.png" alt=""/>
+          
+          <div class="bottom-left"><div className="main"><h5>Unparalleled Sound</h5>
+          <p>Experience crystal-clear audio with premium headphones.</p>
+          <button className='featuredProducts-btn' type="submit">Submit</button>
+          </div>
+          </div>
+ </div>
+          <div className="container3">
+          <img className='b' src="../Assets/girl_with_earphone_image.png" alt=""/>
+          
+          <div class="bottom-left"><div className="main"><h5>Stay Connected</h5>
+          <p>Compact and stylish earphones for every occasion.</p>
+          <button className='featuredProducts-btn' type="submit">Submit</button>
+          </div></div>
+ </div>
+          <div className="container4">
+          <img className='c' src="../Assets/boy_with_laptop_image.png" alt=""/>
+         
+          <div class="bottom-left"> <div className="main"><h5>Power in Every Pixel</h5>
+          <p>Shop the latest laptops for work, gaming and more.</p>
+          <button className='featuredProducts-btn' type="submit">Submit</button>
+          </div>
+          </div>
+ </div>
+
+ </div>
   )
 }
-
 export default FeaturedProducts
