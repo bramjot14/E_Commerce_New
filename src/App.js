9583f144add5bd6def66b6cbf76745d9ab883b9c
bramjot14 <bramjot9874@gmail.com>
2025-09-02 18:44:58 -0400

    Created and completed CallToAction layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import ProductList from './Components/Products/ProductList';
 import FeaturedProducts from './Components/FeaturedProducts/FeaturedProducts';
+import CallToAction from './Components/CallToAction/CallToAction';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <h3 className='featured'>Featured Products</h3>
       <hr class="colorful-line"/>
       <FeaturedProducts></FeaturedProducts>
+      <CallToAction></CallToAction>
     </div>
   );
 }
