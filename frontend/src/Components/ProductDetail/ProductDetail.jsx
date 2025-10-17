296af067451bb6b7d98037f06af2f0938e27aee0
bramjot14 <bramjot9874@gmail.com>
2025-10-17 19:42:59 -0400

    style(ProductDetails): add basic UI styling for each product detail page

--- frontend/src/Components/ProductDetail/ProductDetail.jsx
+++ frontend/src/Components/ProductDetail/ProductDetail.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./ProductDetail.css";
 import { Link, useParams } from 'react-router-dom';
 import { products } from "./Product";
+import { productGallery } from "./ProductGallery";
 
 const ProductDetail = () => {
   const { id } = useParams();
@@ -11,28 +12,44 @@ const ProductDetail = () => {
     return <h2>Product not found</h2>;
   }
 
+  if (product.id == 1) {
+    
+  }
+
   return (
     <div className="product-detail-container">
       <div className="product-detail-item" key={product.id}>
-        <br /><br />
           <div className="left">
           <Link to={`/ProductDetail/${product.id}`}>
             <img src={product.image} className='product-detail-image' alt={product.name} />
+            <Link to={`/ProductDetail/${product.id}`}>
+            <img src={productGallery.image} className='product-detail-image' alt={product.name} />
+            <img src={productGallery.image2} className='product-detail-image' alt={product.name} />
+            <img src={productGallery.image3} className='product-detail-image' alt={product.name} />
+            <img src={productGallery.image4} className='product-detail-image' alt={product.name} />
+            <img src={productGallery.image5} className='product-detail-image' alt={product.name} />
+            </Link>
           </Link>
           </div>
           <div className="right">
-          <h5>{product.name}</h5>
-          <h6>
-            <img src="../Assets/star_Icon.png" alt="" />
-            <img src="../Assets/star_Icon.png" alt="" />
-            <img src="../Assets/star_Icon.png" alt="" />
-            <img src="../Assets/star_Icon.png" alt="" />
-            <img src="../Assets/star_dull_Icon.png" alt="" />
+          <h1>{product.name}</h1>
+          <h4>
+            <img src="../Assets/star_Icon.png" alt="" className='star'/>
+            <img src="../Assets/star_Icon.png" alt="" className='star'/>
+            <img src="../Assets/star_Icon.png" alt="" className='star'/>
+            <img src="../Assets/star_Icon.png" alt="" className='star'/>
+            <img src="../Assets/star_dull_Icon.png" alt="" className='star' />
            ( 4.0 )
-          </h6>
-          <h6 className='product-detail-description'>{product.description}</h6>
-          <h5 className='product-detail-price'>${product.price}</h5>
-          <button className='product-detail-btn'>Buy now</button>
+          </h4>
+          <h4 className='product-detail-description'>{product.description}</h4>
+          <div className="prices">
+          <h1 className='product-detail-price'>${product.price}</h1>
+          <h1 className='product-detail-price2'>${product.originalPrice}</h1>
+          </div>
+          <div className="product-detail-buttons">
+          <button className='product-detail-btn1'>Add to Cart</button>
+          <button className='product-detail-btn2'>Buy now</button>
+          </div>
           </div>
           </div>
         </div>
---
+++ frontend/src/Components/ProductDetail/ProductGallery.js
@@ -0,0 +1,11 @@
+export const productGallery = [
+    {
+      id: 1,
+      image: "../Assets/airpods.png",
+      image2: "../Assets/product_details_page_apple_earphone_image2.png",
+      image3: "../Assets/product_details_page_apple_earphone_image3.png",
+      image4: "../Assets/product_details_page_apple_earphone_image4.png",
+      image5: "../Assets/product_details_page_apple_earphone_image5.png",
+    },
+   ];
+  
