f0285b55982770b3225c96ecff3656350b6a6b08
bramjot14 <bramjot9874@gmail.com>
2025-10-20 16:11:03 -0400

    feature(product-detail): add clickable thumbnails that updates the main image

--- frontend/src/Components/ProductDetail/ProductDetail.jsx
+++ frontend/src/Components/ProductDetail/ProductDetail.jsx
@@ -2,26 +2,41 @@ import React from 'react';
 import "./ProductDetail.css";
 import { Link, useParams } from 'react-router-dom';
 import { products } from "./Product";
+import { useState } from 'react';
+
+
 
 const ProductDetail = () => {
   const { id } = useParams();
   const product = products.find((item) => item.id === Number(id));
+  const [mainImage, setMainImage] = useState(product.Image);
+
+
+
   if (!product) {
     return <h2>Product not found</h2>;
   }
+
+  const handleThumbnailClick = (imageUrl) => {
+    setMainImage(imageUrl);
+  };
 
   return (
     <div className="product-detail-container">
       <div className="product-detail-item" key={product.id}>
       <div className="left">
   <Link to={`/ProductDetail/${product.id}`}>
-    <img src={product.Image} alt="" className='product-detail-image'/>
-    {product.images.map((img, index) => (
-      <img key={index} src={img} className='product-detail-images' alt={product.name} />
+    <img src={mainImage} alt="" className='product-detail-image'/>
+
+    {product.images?.map((img, index) => (
+      <img key={index} src={img} 
+      className='product-detail-images' 
+      alt={`${product.name} thumbnail ${index + 1}`} 
+      onClick={() => handleThumbnailClick(img)} />
     ))}
+
   </Link>
 </div>
-
           <div className="right">
           <h1>{product.name}</h1>
           <h4>
